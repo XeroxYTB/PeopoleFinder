5adafc8db0dd834801208b12b47b18822807d467
XeroxYTB <huglostalatac@gmail.com>
2026-02-09 16:55:59 +0100

    Delete backend/services/insee.js

--- backend/services/insee.js
+++
@@ -1,9 +0,0 @@
-import fetch from "node-fetch";
-
-export async function searchINSEE(lieu) {
-  if (!lieu) return [];
-
-  const url = `https://geo.api.gouv.fr/communes?nom=${lieu}`;
-  const res = await fetch(url);
-  return await res.json();
-}
